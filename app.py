e4ad8e6d115848295531e32cfbbd6b95f686ae5f
Sebastian <sebastian.kowalczykiewicz@mbank.pl>
2023-11-22 18:34:06 +0100

    security fixes with secret.toml

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 import time
 
 import openai
+import hmac
 import streamlit as st
 from utils import load_chain
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Niepoprawne hasło")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 # Custom image for the app icon and the assistant's avatar
 company_logo = 'assets/mbank.png'
